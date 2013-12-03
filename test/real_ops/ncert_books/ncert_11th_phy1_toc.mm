aeac5b61c0eedaa2452d68239bff936612fa37ba
Appan Ponnappan <appan_ponnappan@yahoo.com.sg>
2013-12-03 16:57:54 +0530

    Missed adding this file

---
+++ test/real_ops/ncert_books/ncert_11th_phy1_toc.mm
@@ -0,0 +1,182 @@
+<map version="1.0.0">
+<node CREATED="1347382439772" ID="ID_1" LINK="test/real_ips/ncert_books/ncert_11th_phy1_toc.txt" MODIFIED="1347382510988" TEXT="11-Phy1">
+<node CREATED="1347382439772" ID="ID_2" MODIFIED="1347382510988" TEXT="CHAPTER 1 d PHYSICAL WORLD">
+<node CREATED="1347382439772" ID="ID_3" MODIFIED="1347382510988" TEXT="1.1 What is physics ?:1">
+</node>
+<node CREATED="1347382439772" ID="ID_3_4" MODIFIED="1347382510988" TEXT="1.2 Scope and excitement of physics:2">
+</node>
+<node CREATED="1347382439772" ID="ID_3_5" MODIFIED="1347382510988" TEXT="1.3 Physics, technology and society:5">
+</node>
+<node CREATED="1347382439772" ID="ID_3_6" MODIFIED="1347382510988" TEXT="1.4 Fundamental forces in nature:6">
+</node>
+<node CREATED="1347382439772" ID="ID_3_7" MODIFIED="1347382510988" TEXT="1.5 Nature of physical laws:10">
+</node>
+</node>
+<node CREATED="1347382439772" ID="ID_8" MODIFIED="1347382510988" TEXT="CHAPTER 2 UNITS AND MEASUREMENTS E u">
+<node CREATED="1347382439772" ID="ID_9" MODIFIED="1347382510988" TEXT="2.1 Introduction:16">
+</node>
+<node CREATED="1347382439772" ID="ID_3_10" MODIFIED="1347382510988" TEXT="2.2 The international system of units:16">
+</node>
+<node CREATED="1347382439772" ID="ID_3_11" MODIFIED="1347382510988" TEXT="2.3 Measurement of length:18">
+</node>
+<node CREATED="1347382439772" ID="ID_3_12" MODIFIED="1347382510988" TEXT="2.4 Measurement of mass:21">
+</node>
+<node CREATED="1347382439772" ID="ID_3_13" MODIFIED="1347382510988" TEXT="2.5 Measurement of time:22">
+</node>
+<node CREATED="1347382439772" ID="ID_3_14" MODIFIED="1347382510988" TEXT="2.6 Accuracy, precision of instruments and errors in measurement:22">
+</node>
+<node CREATED="1347382439772" ID="ID_3_15" MODIFIED="1347382510988" TEXT="2.7 Significant figures:27">
+</node>
+<node CREATED="1347382439772" ID="ID_3_16" MODIFIED="1347382510988" TEXT="2.8 Dimensions of physical quantities:31">
+</node>
+<node CREATED="1347382439772" ID="ID_3_17" MODIFIED="1347382510988" TEXT="2.9 Dimensional formulae and dimensional equations:31">
+</node>
+<node CREATED="1347382439772" ID="ID_3_18" MODIFIED="1347382510988" TEXT="2.10 Dimensional analysis and its applications:32">
+</node>
+</node>
+<node CREATED="1347382439772" ID="ID_19" MODIFIED="1347382510988" TEXT="CHAPTER 3 to MOTION IN A STRAIGHT LINE">
+<node CREATED="1347382439772" ID="ID_20" MODIFIED="1347382510988" TEXT="3.1 Introduction:39">
+</node>
+<node CREATED="1347382439772" ID="ID_3_21" MODIFIED="1347382510988" TEXT="3.2 Position, path length and displacement:39">
+</node>
+<node CREATED="1347382439772" ID="ID_3_22" MODIFIED="1347382510988" TEXT="3.3 Average velocity and average speed:42">
+</node>
+<node CREATED="1347382439772" ID="ID_3_23" MODIFIED="1347382510988" TEXT="3.4 Instantaneous velocity and speed:43">
+</node>
+<node CREATED="1347382439772" ID="ID_3_24" MODIFIED="1347382510988" TEXT="3.5 Acceleration:45">
+</node>
+<node CREATED="1347382439772" ID="ID_3_25" MODIFIED="1347382510988" TEXT="3.6 Kinematic equations for uniformly accelerated motion:47">
+</node>
+<node CREATED="1347382439772" ID="ID_3_26" MODIFIED="1347382510988" TEXT="3.7 Relative velocity:51">
+</node>
+</node>
+<node CREATED="1347382439772" ID="ID_27" MODIFIED="1347382510988" TEXT="CHAPTER 4 MOTION IN A PLANE">
+<node CREATED="1347382439772" ID="ID_28" MODIFIED="1347382510988" TEXT="4.1 Introduction:65">
+</node>
+<node CREATED="1347382439772" ID="ID_3_29" MODIFIED="1347382510988" TEXT="4.2 Scalars and vectors:65">
+</node>
+<node CREATED="1347382439772" ID="ID_3_30" MODIFIED="1347382510988" TEXT="4.3 Multiplication of vectors by real numbers:67">
+</node>
+<node CREATED="1347382439772" ID="ID_3_31" MODIFIED="1347382510988" TEXT="4.4 Addition and subtraction of vectors - graphical method:67">
+</node>
+<node CREATED="1347382439772" ID="ID_3_32" MODIFIED="1347382510988" TEXT="4.5 Resolution of vectors:69">
+</node>
+<node CREATED="1347382439772" ID="ID_3_33" MODIFIED="1347382510988" TEXT="4.6 Vector addition - analytical method:71">
+</node>
+<node CREATED="1347382439772" ID="ID_3_34" MODIFIED="1347382510988" TEXT="4.7 Motion in a plane:72">
+</node>
+<node CREATED="1347382439772" ID="ID_3_35" MODIFIED="1347382510988" TEXT="4.8 Motion in a plane with constant acceleration:75">
+</node>
+<node CREATED="1347382439772" ID="ID_3_36" MODIFIED="1347382510988" TEXT="4.9 Relative velocity in two dimensions:76">
+</node>
+<node CREATED="1347382439772" ID="ID_3_37" MODIFIED="1347382510988" TEXT="4.10 Projectile motion:77">
+</node>
+<node CREATED="1347382439772" ID="ID_3_38" MODIFIED="1347382510988" TEXT="4.11 Uniform circular motion:79">
+</node>
+</node>
+<node CREATED="1347382439772" ID="ID_39" MODIFIED="1347382510988" TEXT="CHAPTER 5 LAWS OF MOTION d">
+<node CREATED="1347382439772" ID="ID_40" MODIFIED="1347382510988" TEXT="5.1 Introduction:89">
+</node>
+<node CREATED="1347382439772" ID="ID_3_41" MODIFIED="1347382510988" TEXT="5.2 Aristotle's fallacy:90">
+</node>
+<node CREATED="1347382439772" ID="ID_3_42" MODIFIED="1347382510988" TEXT="5.3 The law of inertia:90">
+</node>
+<node CREATED="1347382439772" ID="ID_3_43" MODIFIED="1347382510988" TEXT="5.4 Newton's first law of motion:91">
+</node>
+<node CREATED="1347382439772" ID="ID_3_44" MODIFIED="1347382510988" TEXT="5.5 Newton's second law of motion:93">
+</node>
+<node CREATED="1347382439772" ID="ID_3_45" MODIFIED="1347382510988" TEXT="5.7 Conservation of momentum:98">
+</node>
+<node CREATED="1347382439772" ID="ID_3_46" MODIFIED="1347382510988" TEXT="5.8 Equilibrium of a particle:99">
+</node>
+<node CREATED="1347382439772" ID="ID_3_47" MODIFIED="1347382510988" TEXT="5.9 Common forces in mechanics:100">
+</node>
+<node CREATED="1347382439772" ID="ID_3_48" MODIFIED="1347382510988" TEXT="5.10 Circular motion:104">
+</node>
+<node CREATED="1347382439772" ID="ID_3_49" MODIFIED="1347382510988" TEXT="5.11 Solving problems in mechanics:105">
+</node>
+</node>
+<node CREATED="1347382439772" ID="ID_50" MODIFIED="1347382510988" TEXT="CHAPTER 6 WORK, ENERGY AND POWER N re">
+<node CREATED="1347382439772" ID="ID_51" MODIFIED="1347382510988" TEXT="6.1 Introduction:114">
+</node>
+<node CREATED="1347382439772" ID="ID_3_52" MODIFIED="1347382510988" TEXT="6.2 Notions of work and kinetic energy : The work-energy theorem:116">
+</node>
+<node CREATED="1347382439772" ID="ID_3_53" MODIFIED="1347382510988" TEXT="6.3 Work:116">
+</node>
+<node CREATED="1347382439772" ID="ID_3_54" MODIFIED="1347382510988" TEXT="6.4 Kinetic energy:117">
+</node>
+<node CREATED="1347382439772" ID="ID_3_55" MODIFIED="1347382510988" TEXT="6.5 Work done by a variable force:118">
+</node>
+<node CREATED="1347382439772" ID="ID_3_56" MODIFIED="1347382510988" TEXT="6.6 The work-energy theorem for a variable force:119">
+</node>
+<node CREATED="1347382439772" ID="ID_3_57" MODIFIED="1347382510988" TEXT="6.7 The concept of potential energy:120">
+</node>
+<node CREATED="1347382439772" ID="ID_3_58" MODIFIED="1347382510988" TEXT="6.8 The conservation of mechanical energy:121">
+</node>
+<node CREATED="1347382439772" ID="ID_3_59" MODIFIED="1347382510988" TEXT="6.9 The potential energy of a spring:123">
+</node>
+<node CREATED="1347382439772" ID="ID_3_60" MODIFIED="1347382510988" TEXT="6.10 Various forms of energy : the law of conservation of energy:126">
+</node>
+<node CREATED="1347382439772" ID="ID_3_61" MODIFIED="1347382510988" TEXT="6.11 Power:128">
+</node>
+<node CREATED="1347382439772" ID="ID_3_62" MODIFIED="1347382510988" TEXT="6.12 Collisions:129">
+</node>
+</node>
+<node CREATED="1347382439772" ID="ID_63" MODIFIED="1347382510988" TEXT="CHAPTER 7 SYSTEM OF PARTICLES AND ROTATIONAL MOTION n">
+<node CREATED="1347382439772" ID="ID_64" MODIFIED="1347382510988" TEXT="7.1 Introduction:141">
+</node>
+<node CREATED="1347382439772" ID="ID_3_65" MODIFIED="1347382510988" TEXT="7.2 Centre of mass:144">
+</node>
+<node CREATED="1347382439772" ID="ID_3_66" MODIFIED="1347382510988" TEXT="7.3 Motion of centre of mass:148">
+</node>
+<node CREATED="1347382439772" ID="ID_3_67" MODIFIED="1347382510988" TEXT="7.4 Linear momentum of a system of particles:149">
+</node>
+<node CREATED="1347382439772" ID="ID_3_68" MODIFIED="1347382510988" TEXT="7.5 Vector product of two vectors:150">
+</node>
+<node CREATED="1347382439772" ID="ID_3_69" MODIFIED="1347382510988" TEXT="7.6 Angular velocity and its relation with linear velocity:152">
+</node>
+<node CREATED="1347382439772" ID="ID_3_70" MODIFIED="1347382510988" TEXT="7.7 Torque and angular momentum:154">
+</node>
+<node CREATED="1347382439772" ID="ID_3_71" MODIFIED="1347382510988" TEXT="7.8 Equilibrium of a rigid body:158">
+</node>
+<node CREATED="1347382439772" ID="ID_3_72" MODIFIED="1347382510988" TEXT="7.9 Moment of inertia:163">
+</node>
+<node CREATED="1347382439772" ID="ID_3_73" MODIFIED="1347382510988" TEXT="7.10 Theorems of perpendicular and parallel axes:164">
+</node>
+<node CREATED="1347382439772" ID="ID_3_74" MODIFIED="1347382510988" TEXT="7.11 Kinematics of rotational motion about a fixed axis:167">
+</node>
+<node CREATED="1347382439772" ID="ID_3_75" MODIFIED="1347382510988" TEXT="7.12 Dynamics of rotational motion about a fixed axis:169">
+</node>
+<node CREATED="1347382439772" ID="ID_3_76" MODIFIED="1347382510988" TEXT="7.13 Angular momentum in case of rotations about a fixed axis:171">
+</node>
+<node CREATED="1347382439772" ID="ID_3_77" MODIFIED="1347382510988" TEXT="7.14 Rolling motion:173">
+</node>
+</node>
+<node CREATED="1347382439772" ID="ID_78" MODIFIED="1347382510988" TEXT="CHAPTER 8 GRAVITATION">
+<node CREATED="1347382439772" ID="ID_79" MODIFIED="1347382510988" TEXT="8.1 Introduction:183">
+</node>
+<node CREATED="1347382439772" ID="ID_3_80" MODIFIED="1347382510988" TEXT="8.2 Kepler's laws:184">
+</node>
+<node CREATED="1347382439772" ID="ID_3_81" MODIFIED="1347382510988" TEXT="8.3 Universal law of gravitation:185">
+</node>
+<node CREATED="1347382439772" ID="ID_3_82" MODIFIED="1347382510988" TEXT="8.4 The gravitational constant:189">
+</node>
+<node CREATED="1347382439772" ID="ID_3_83" MODIFIED="1347382510988" TEXT="8.5 Acceleration due to gravity of the earth:189">
+</node>
+<node CREATED="1347382439772" ID="ID_3_84" MODIFIED="1347382510988" TEXT="8.6 Acceleration due to gravity below and above the surface of earth:190">
+</node>
+<node CREATED="1347382439772" ID="ID_3_85" MODIFIED="1347382510988" TEXT="8.7 Gravitational potential energy:191">
+</node>
+<node CREATED="1347382439772" ID="ID_3_86" MODIFIED="1347382510988" TEXT="8.8 Escape speed:193">
+</node>
+<node CREATED="1347382439772" ID="ID_3_87" MODIFIED="1347382510988" TEXT="8.9 Earth satellite:194">
+</node>
+<node CREATED="1347382439772" ID="ID_3_88" MODIFIED="1347382510988" TEXT="8.10 Energy of an orbiting satellite:195">
+</node>
+<node CREATED="1347382439772" ID="ID_3_89" MODIFIED="1347382510988" TEXT="8.11 Geostationary and polar satellites:196">
+</node>
+<node CREATED="1347382439772" ID="ID_3_90" MODIFIED="1347382510988" TEXT="8.12 Weightlessness:197">
+</node>
+</node>
+</node>
+</map>
